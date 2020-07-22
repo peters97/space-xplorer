b39f6f0ac22cb263cc4524c93ec287722922ae2f
Daan Peters <daan@lyfter.nl>
2020-07-22 21:21:00 +0200

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Main from "./components/Main";
+import Nav from "./components/Nav";
 
 function App() {
-  return (
-    <div className="App">
-      <Main/>
-    </div>
-  );
+    return (
+        <div className="h-screen w-full flex overflow-hidden select-none">
+            <Nav/>
+            <main
+                className="my-1 pt-2 pb-2 px-10 flex-1 bg-gray-200 dark:bg-black rounded-l-lg
+    transition duration-500 ease-in-out overflow-y-auto">
+                <Main/>
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/components/cards/LauchCard.tsx
+++ src/components/cards/LauchCard.tsx
@@ -7,7 +7,7 @@ const LaunchCard = ({launch}) => {
     console.log(launch);
     return (
         <div className="w-full sm:w-1/2 lg:w-1/4 flex mb-4 px-2">
-            <div className="flex-1 rounded shadow-lg">
+            <div className="flex-1 rounded bg-white shadow-lg">
                 <div className="p-10">
                     <h3 className="mb-2 text-xl font-bold text-purple-800">{launch.mission_name}</h3>
                     <p className="text-gray-700 font-mono">{truncateString(launch.details, 20)}</p>
