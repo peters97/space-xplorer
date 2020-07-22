ef93776a448753b1bbb6cb3dc0cb6fb1d76ea516
Daan Peters <daan@lyfter.nl>
2020-07-22 22:24:30 +0200

    Updates

---
+++ precache-manifest.16155647add466f25ef2653ff6521550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b59d01c91459bea5a6ad2eb8338133a",
+    "url": "/space-xplorer/index.html"
+  },
+  {
+    "revision": "217e64e647a53d98ad95",
+    "url": "/space-xplorer/static/css/main.42c34c13.chunk.css"
+  },
+  {
+    "revision": "f557e95113571544a81f",
+    "url": "/space-xplorer/static/js/2.2eefc8f9.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/space-xplorer/static/js/2.2eefc8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217e64e647a53d98ad95",
+    "url": "/space-xplorer/static/js/main.7c4bb7ee.chunk.js"
+  },
+  {
+    "revision": "31ad5cd8d586e7c1c181",
+    "url": "/space-xplorer/static/js/runtime-main.6076a61e.js"
+  }
+]);
