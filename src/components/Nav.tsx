34a278bba288ade923773c67d94e7b287fe83ca2
Daan Peters <daan@lyfter.nl>
2020-07-22 22:36:14 +0200

    Cleanup nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faHouse, faPlanetRinged, faRocket} from "@fortawesome/pro-regular-svg-icons";
-import { Link, NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 
 const Nav = () => {
@@ -27,20 +27,6 @@ const Nav = () => {
                 </NavLink>
 
             </ul>
-
-            {/*<div className="mt-auto flex items-center p-2 text-blue-700 bg-purple-200 dark:text-blue-500 rounded-full">*/}
-
-            {/*    <a href="/">*/}
-            {/*        <svg className="fill-current h-5 w-5" viewBox="0 0 24 24">*/}
-            {/*            <path*/}
-            {/*                d="M12 1c-5 0-9 4-9 9v7a3 3 0 003 3h3v-8H5v-2a7 7 0 017-7*/}
-            {/*7 7 0 017 7v2h-4v8h4v1h-7v2h6a3 3 0*/}
-            {/*003-3V10c0-5-4.03-9-9-9z"></path>*/}
-            {/*        </svg>*/}
-            {/*    </a>*/}
-
-            {/*</div>*/}
-
         </nav>
     );
 }
